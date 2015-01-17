a2763a6851025554ce44a9b7c60c0ba6a7921824
Yukang <moorekang@gmail.com>
2015-01-17 11:28:24 +0800

    tunning bench

--- src/env.rs
+++ src/env.rs
@@ -309,7 +309,7 @@ fn test_env_parent() {
 fn env_bench(b: &mut Bencher) {
     fn test_env() {
         let mut env = Env::new();
-        for i in 1..1000 {
+        for i in 1..10000 {
             let key = i.to_string();
             env.def_var(key.clone(), Expr::Str(StrNode::new("world")));
             let val = env.lookup(key);
