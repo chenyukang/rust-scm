8a09aadacd89ff54b013dcd616af4c0ba6da1d7f
Yukang <moorekang@gmail.com>
2015-01-19 23:27:23 +0800

    trivial fix

--- src/eval.rs
+++ src/eval.rs
@@ -285,8 +285,8 @@ fn test_evaler() {
     test_case!("(begin 1 2)", as_int, 2);
     test_case!("((lambda (x) x) 5)", as_int, 5);
     test_case!("(let ((fu (lambda (x) (+ x 1)))) (fu 1))", as_int, 2);
-    test_case!("((lambda ( x y ) ( if ( = y 0) 1 (* y (x x (- y 1)))))
-               (lambda ( x y ) ( if ( = y 0) 1 (* y (x x (- y 1))))) 5)", as_int, 5is*4*3*2);
+    test_case!("((lambda (x y ) (if ( = y 0) 1 (* y (x x (- y 1)))))
+               (lambda (x y ) (if ( = y 0) 1 (* y (x x (- y 1))))) 5)", as_int, 5is*4*3*2);
 }
 
 #[bench]
@@ -297,6 +297,6 @@ fn eval_bench(b: &mut Bencher) {
 #[bench]
 fn lambda_bench(b: &mut Bencher) {
     b.iter(||
-           test_case!("((lambda ( x y ) ( if ( = y 0) 1 (* y (x x (- y 1)))))
-                      (lambda ( x y ) ( if ( = y 0) 1 (* y (x x (- y 1))))) 5)", as_int, 5is*4*3*2));
+           test_case!("((lambda (x y ) (if ( = y 0) 1 (* y (x x (- y 1)))))
+                      (lambda (x y ) (if ( = y 0) 1 (* y (x x (- y 1))))) 5)", as_int, 5is*4*3*2));
 }
